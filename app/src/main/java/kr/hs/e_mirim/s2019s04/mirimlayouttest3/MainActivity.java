7cb806f4fab310d2d3428d5a5118406ad06c6161
Kwon Hye Soo <kwon7788966@naver.com>
2020-10-12 19:51:55 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/e_mirim/s2019s04/mirimlayouttest3/MainActivity.java
@@ -0,0 +1,41 @@
+package kr.hs.e_mirim.s2019s04.mirimlayouttest3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout linear1 = new LinearLayout(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+
+        linear1.setBackgroundColor(Color.rgb(181, 178, 255));
+        linear1.setOrientation(LinearLayout.VERTICAL);
+
+        Button btn = new Button(this);
+        btn.setText("클릭");
+        btn.setBackgroundColor(Color.GRAY);
+        btn.setTextColor(Color.WHITE);
+        linear1.addView(btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "java 코드로 작성한 버튼 입니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        setContentView(linear1, params);
+    }
+}
